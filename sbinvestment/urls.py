67bcf3a0bf950b0bda31bab42346b9b3fbd65640
YOONBH <bhyoon24@naver.com>
2023-02-14 23:52:09 +0900

    change menu

--- sbinvestment/urls.py
+++ sbinvestment/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.sms, name="sms"),
+    path('index', views.index, name='index'),
     path('member', views.sb_member, name='sb-member'),
-    path('sms', views.sms, name="sms"),
 ]
